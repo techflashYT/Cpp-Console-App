940183bb009b53236294eae3a6b5dce87c79eb76
Techflash <michaelfgarofalo@gmail.com>
2022-02-12 14:28:19 -0800

    Update source files
    Update the C++ source files to add some new things.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
+#include "inc/main.h"
+
 int main(int argc, char **argv) {
     // Fool the compiler into thinking I'm using the variables until I find a use for them.
     (void)argc;
     (void)argv;
+    #ifdef debugMode
+        // TODO: Do debug mode things
+    #endif
     return 0;
 }
