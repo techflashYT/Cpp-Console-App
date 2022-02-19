20c5c61c3022ce01099941d527055fe45c73605e
Techflash <michaelfgarofalo@gmail.com>
2022-02-18 22:49:14 -0800

    Fix indentation
    Update indentation to tabs across all files.

--- src/CMD_Args.cpp
+++ src/CMD_Args.cpp
@@ -1,15 +1,15 @@
 #include "inc/CMD_Args.hpp"
 
 void parseArgs(int argc, char** argv, bool* debugMode) {
-    // Check if argument is --debug-mode
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "--debug-mode") == 0) {
-            *debugMode = true;
-        }
-        else {
-            printf_s("Invalid argument!\r\n");
-            // showUsage();
-            std::exit(1);
-        }
-    }
+	// Check if argument is --debug-mode
+	for (int i = 1; i < argc; i++) {
+		if (strcmp(argv[i], "--debug-mode") == 0) {
+			*debugMode = true;
+		}
+		else {
+			printf_s("Invalid argument!\r\n");
+			// showUsage();
+			std::exit(1);
+		}
+	}
 }
--- src/writeNameToFile.cpp
+++ src/writeNameToFile.cpp
@@ -1,37 +1,37 @@
 #include "inc/writeNameToFile.hpp"
 #include "inc/defines.hpp"
 uint8_t writeNameToFile(char name[50], std::string pathString, std::string origPath, bool debugMode) {
-    pathString = origPath + "\\CONFIG\\NAME";
-    if (debugMode) {
-        printf_s("%sOpening std::ofstream filestream using the path %s\r\n", dbgPrefix, pathString.c_str());
-    }
-    std::ofstream fileStream(pathString);
-    if (fileStream.is_open()) {
-        std::string nameToWrite = name;
-        nameToWrite.substr(nameToWrite.find("\0") + 1);
-        if (debugMode) {
-            printf_s("%snameToWrite value: %s\r\n", dbgPrefix, nameToWrite.c_str());
-            printf_s("%sSuccessfully opened the fileStream, writing data \"%s\" to it with a maximum of 50 characters.\r\n", dbgPrefix, nameToWrite.c_str());
-        }
-        fileStream.write(nameToWrite.c_str(), nameToWrite.size());
-    }
-    else {
-        return 1;
-    }
-    if (debugMode) {
-        printf_s("%sDone writing, now closing the stream.\r\n", dbgPrefix);
-    }
-    fileStream.close();
-    if (fileStream.good()) {
-        if (debugMode) {
-            printf_s("%sFilestream wrote and closed correctly!  Returning 0 to show that it succeeded.\r\n", dbgPrefix);
-        }
-        return 0;
-    }
-    else {
-        if (debugMode) {
-            printf_s("%sFilestream failed to write or close!  Returning 1 to show that it failed.\r\n", dbgPrefix);
-        }
-        return 1;
-    }
+	pathString = origPath + "\\CONFIG\\NAME";
+	if (debugMode) {
+		printf_s("%sOpening std::ofstream filestream using the path %s\r\n", dbgPrefix, pathString.c_str());
+	}
+	std::ofstream fileStream(pathString);
+	if (fileStream.is_open()) {
+		std::string nameToWrite = name;
+		nameToWrite.substr(nameToWrite.find("\0") + 1);
+		if (debugMode) {
+			printf_s("%snameToWrite value: %s\r\n", dbgPrefix, nameToWrite.c_str());
+			printf_s("%sSuccessfully opened the fileStream, writing data \"%s\" to it with a maximum of 50 characters.\r\n", dbgPrefix, nameToWrite.c_str());
+		}
+		fileStream.write(nameToWrite.c_str(), nameToWrite.size());
+	}
+	else {
+		return 1;
+	}
+	if (debugMode) {
+		printf_s("%sDone writing, now closing the stream.\r\n", dbgPrefix);
+	}
+	fileStream.close();
+	if (fileStream.good()) {
+		if (debugMode) {
+			printf_s("%sFilestream wrote and closed correctly!  Returning 0 to show that it succeeded.\r\n", dbgPrefix);
+		}
+		return 0;
+	}
+	else {
+		if (debugMode) {
+			printf_s("%sFilestream failed to write or close!  Returning 1 to show that it failed.\r\n", dbgPrefix);
+		}
+		return 1;
+	}
 }
