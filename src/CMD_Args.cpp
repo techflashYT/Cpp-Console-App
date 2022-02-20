77f3196b092ee815a6fb10ac723604aeae1e767c
Techflash <michaelfgarofalo@gmail.com>
2022-02-19 16:47:11 -0800

    Add args for debugger
    Add arguments that are used by the GDB debugger

--- src/CMD_Args.cpp
+++ src/CMD_Args.cpp
@@ -2,14 +2,34 @@
 
 void parseArgs(int_fast32_t argc, char** argv, bool* debugMode) {
 	// Check if argument is --debug-mode
+	bool alreadyShownMsg = false;
 	for (uint_fast8_t i = 1; i < argc; i++) {
 		if (strcmp(argv[i], "--debug-mode") == 0) {
 			*debugMode = true;
 		}
+		else if (strcmp(argv[i], "2>CON") == 0) {
+			if (!alreadyShownMsg) {
+				printf_s("Thanks for using a debugger to help test the program!\r\n");
+			}
+			alreadyShownMsg = true;
+		}
+		else if (strcmp(argv[i], "1>CON") == 0) {
+			if (!alreadyShownMsg) {
+				printf_s("Thanks for using a debugger to help test the program!\r\n");
+			}
+			alreadyShownMsg = true;
+		}
+		else if (strcmp(argv[i], "<CON") == 0) {
+			if (!alreadyShownMsg) {
+				printf_s("Thanks for using a debugger to help test the program!\r\n");
+			}
+			alreadyShownMsg = true;
+		}
 		else {
-			printf_s("Invalid argument!\r\n");
+			printf_s("Invalid argument: \"%s\"!\r\n", argv[i]);
 			// showUsage();
 			std::exit(1);
 		}
 	}
 }
+
